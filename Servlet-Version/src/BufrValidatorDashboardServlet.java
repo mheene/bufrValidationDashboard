d5fe53715aab4270a3d12c84d25ec7658148cc36
Markus Heene <markus.heene@gmail.com>
2018-05-21 08:17:34 +0200

    Additional Get-Request for ECMWF

--- Servlet-Version/src/BufrValidatorDashboardServlet.java
+++ Servlet-Version/src/BufrValidatorDashboardServlet.java
@@ -8,10 +8,14 @@ import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+
 import java.util.concurrent.Callable;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import java.util.concurrent.FutureTask;
+
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -38,8 +42,6 @@ import org.apache.http.entity.mime.content.StringBody;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
-
-
 
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
@@ -72,7 +74,7 @@ public class BufrValidatorDashboardServlet extends HttpServlet {
      * upload data and saves the file on disk.
      */
     protected void doPost(HttpServletRequest request,
-            HttpServletResponse response) throws ServletException, IOException {
+			  HttpServletResponse response) throws ServletException, IOException {
         // checks if the request actually contains upload file
 	// error.jsp noch einbauen
         if (!ServletFileUpload.isMultipartContent(request)) {
@@ -110,10 +112,10 @@ public class BufrValidatorDashboardServlet extends HttpServlet {
          
 	// creates the directory if it does not exist
 	if (Boolean.valueOf(getServletConfig().getInitParameter("storeFiles"))) {
-		uploadDir = new File(uploadPath);
-		if (!uploadDir.exists()) {
-		    uploadDir.mkdir();
-		}
+	    uploadDir = new File(uploadPath);
+	    if (!uploadDir.exists()) {
+		uploadDir.mkdir();
+	    }
 	}
 
  
@@ -128,95 +130,111 @@ public class BufrValidatorDashboardServlet extends HttpServlet {
                     // processes only fields that are not form fields
                     if (!item.isFormField()) {
                         String fileName = new File(item.getName()).getName();
-						if (Boolean.valueOf(getServletConfig().getInitParameter("storeFiles"))) {
-							String filePath = uploadPath + File.separator + fileName;
-							File storeFile = new File(filePath);
-							// saves the file on disk; old files with same filename are overwritten
-							item.write(storeFile);
-						}
-						//process bufr
-						InputStream is = item.getInputStream();
-						
-						File tempFile = File.createTempFile("prefix-", "-suffix");
-						//tempFile.deleteOnExit();
-						FileOutputStream out = new FileOutputStream(tempFile);
-						IOUtils.copy(is, out);
-						out.flush();
-						
-						StringBuffer sb = new StringBuffer();
-						executor = Executors.newFixedThreadPool(4);
-						
-						//List<GetRequestTask> tasks = new ArrayList<GetRequestTask>();
-						List<IfcRequestTask> tasks = new ArrayList<IfcRequestTask>();
-						//tasks.add(new GetRequestTask("http://localhost:8080/examples/jsp/jsp2/el/basic-arithmetic.jsp", this.executor));
-						//tasks.add(new GetRequestTask("http://localhost:8080/examples/jsp/jsp2/el/basic-comparisons.jsp", this.executor));
-						tasks.add(new PostRequestTask("http://apps.ecmwf.int/codes/bufr/validator/", fileName, "filebox", tempFile, this.executor));
-						tasks.add(new PostRequestTask("https://kunden.dwd.de/bufrviewer/uploadFile", fileName, "uploadFile",tempFile, this.executor));
-						//...
-						//do other work here
-						//...
-						//now wait for all async tasks to complete
-						while(!tasks.isEmpty()) {
-							//for(Iterator<GetRequestTask> it = tasks.iterator(); it.hasNext();) {
-							for(Iterator<IfcRequestTask> it = tasks.iterator(); it.hasNext();) {
-								//GetRequestTask task = it.next();
-								IfcRequestTask task = it.next();
-								if(task.isDone()) {
-									String p_request = task.getRequest();
-									String p_response = task.getResponse();
-									//PUT YOUR CODE HERE
-									//possibly aggregate request and response in Map<String,String>
-									//or do something else with request and response
-									sb.append("Request: " + p_request + "\n");
-									sb.append("Response: " + p_response + "\n");
-									it.remove();
-								}
-							}
-							//avoid tight loop in "main" thread
-							if(!tasks.isEmpty()) Thread.sleep(100);
-						}
-						//now you have all responses for all async requests
-
-						//the following from your original code
-						//note: you should probably pass the responses from above
-						//to this next method (to keep your controller stateless)
-						//String results = doWorkwithMultipleDataReturned();
-						request.setAttribute("bufr", sb.toString()); 
-						//model.addAttribute(results, results);
-						//return "index";
-
-						
-						
-					}
-				}
+			if (Boolean.valueOf(getServletConfig().getInitParameter("storeFiles"))) {
+			    String filePath = uploadPath + File.separator + fileName;
+			    File storeFile = new File(filePath);
+			    // saves the file on disk; old files with same filename are overwritten
+			    item.write(storeFile);
 			}
+			//process bufr
+			InputStream is = item.getInputStream();
+						
+			File tempFile = File.createTempFile("prefix-", "-suffix");
+			//tempFile.deleteOnExit();
+			FileOutputStream out = new FileOutputStream(tempFile);
+			IOUtils.copy(is, out);
+			out.flush();
+						
+			StringBuffer sb = new StringBuffer();
+			executor = Executors.newFixedThreadPool(4);
+						
+			//List<GetRequestTask> tasks = new ArrayList<GetRequestTask>();
+			List<IfcRequestTask> tasks = new ArrayList<IfcRequestTask>();
+			//tasks.add(new GetRequestTask("http://localhost:8080/examples/jsp/jsp2/el/basic-arithmetic.jsp", this.executor));
+			//tasks.add(new GetRequestTask("http://localhost:8080/examples/jsp/jsp2/el/basic-comparisons.jsp", this.executor));
+			tasks.add(new PostRequestTask("http://apps.ecmwf.int/codes/bufr/validator/", fileName, "filebox", tempFile, this.executor));
+			tasks.add(new PostRequestTask("https://kunden.dwd.de/bufrviewer/uploadFile", fileName, "uploadFile",tempFile, this.executor));
+			//...
+			//do other work here
+			//...
+			//now wait for all async tasks to complete
+			Pattern p = Pattern.compile("https\\://stream\\.ecmwf\\.int.*json");
+
+			while(!tasks.isEmpty()) {
+			    //for(Iterator<GetRequestTask> it = tasks.iterator(); it.hasNext();) {
+			    String url = null;
+			    boolean foundUrl = false;
+			    for(Iterator<IfcRequestTask> it = tasks.iterator(); it.hasNext();) {
+				//GetRequestTask task = it.next();
+				IfcRequestTask task = it.next();
+				if(task.isDone()) {
+				    String p_request = task.getRequest();
+				    String p_response = task.getResponse();
+				    Matcher m = p.matcher(p_response);
+				    //System.out.println("Matcher ECMWF: " + m.group());
+				    //PUT YOUR CODE HERE
+				    //possibly aggregate request and response in Map<String,String>
+				    //or do something else with request and response
+				    sb.append("Request: " + p_request + "\n");
+				    sb.append("Response: " + p_response + "\n");
+				    if (m.find()) {
+					url = m.group();
+					foundUrl = true;
+					//sb.append("Matcher ECMWF: " + m.group());
+
+				    }
+				    it.remove();
+				}
+			    }
+			    //avoid tight loop in "main" thread
+			    if(!tasks.isEmpty()) Thread.sleep(100);
+			    if (foundUrl) {
+				tasks.add(new GetRequestTask(url, this.executor));
+				Thread.sleep(100);
+			    }
+			}
+			//now you have all responses for all async requests
+
+			//the following from your original code
+			//note: you should probably pass the responses from above
+			//to this next method (to keep your controller stateless)
+			//String results = doWorkwithMultipleDataReturned();
+			request.setAttribute("bufr", sb.toString()); 
+			//model.addAttribute(results, results);
+			//return "index";
+
+						
+						
+		    }
+		}
+	    }
             
 
         } catch (Exception ex) {
 	    
-			System.out.println("ex: 2 " + ex.getMessage());
-			System.out.println("ex: 2 " + ex.getClass().getName());
-			getServletContext().getRequestDispatcher("/error.jsp").forward(
-                request, response);
-				return;
-	    }
+	    System.out.println("ex: 2 " + ex.getMessage());
+	    System.out.println("ex: 2 " + ex.getClass().getName());
+	    getServletContext().getRequestDispatcher("/error.jsp").forward(
+									   request, response);
+	    return;
+	}
 		
 		
 
 
 	getServletContext().getRequestDispatcher("/upload.jsp").forward(
-                request, response);
+									request, response);
 
 
     }
 	
-	interface IfcRequestTask {
-		public String getRequest();
-		public boolean isDone();
-		public String getResponse();
-	}
+    interface IfcRequestTask {
+	public String getRequest();
+	public boolean isDone();
+	public String getResponse();
+    }
 	
-	class GetRequestTask implements IfcRequestTask {
+    class GetRequestTask implements IfcRequestTask {
         private GetRequestWork work;
         private FutureTask<String> task;
         public GetRequestTask(String url, Executor executor) {
@@ -253,7 +271,7 @@ public class BufrValidatorDashboardServlet extends HttpServlet {
         }
     }
 	
-	class PostRequestTask implements IfcRequestTask{
+    class PostRequestTask implements IfcRequestTask{
         private PostRequestWork work;
         private FutureTask<String> task;
 		
@@ -277,108 +295,108 @@ public class BufrValidatorDashboardServlet extends HttpServlet {
         }
     }
 	
-	//Callable representing actual HTTP POST request
+    //Callable representing actual HTTP POST request
     class PostRequestWork implements Callable<String> {
         private final String url;
-		private final String fileName;
-		private final String fieldName;
-		private File file;
+	private final String fileName;
+	private final String fieldName;
+	private File file;
 		
-		/*
-        public PostRequestWork(String url) {
-            this.url = url;
-        }
-		*/
+	/*
+	  public PostRequestWork(String url) {
+	  this.url = url;
+	  }
+	*/
 		
-		public PostRequestWork(String url, String fileName, String fieldName, File file) {
+	public PostRequestWork(String url, String fileName, String fieldName, File file) {
             this.url = url;
-			this.fileName = fileName;
-			this.fieldName = fieldName;
-			this.file = file;
+	    this.fileName = fileName;
+	    this.fieldName = fieldName;
+	    this.file = file;
         }
 		
         public String getUrl() {
             return this.url;
         }
 		
-		public String getFileName() {
-			return this.fileName;
-		}
+	public String getFileName() {
+	    return this.fileName;
+	}
 		
-		public String getFieldName() {
-			return this.fieldName;
-		}
+	public String getFieldName() {
+	    return this.fieldName;
+	}
 		
-		public File getFile() {
-			return this.file;
-		}
+	public File getFile() {
+	    return this.file;
+	}
 		
         public String call() throws Exception {
 			
-			CloseableHttpClient httpclient = HttpClients.createDefault();
-			String returnValue = "";
-			try {
-				HttpPost httppost = new HttpPost(getUrl());
+	    CloseableHttpClient httpclient = HttpClients.createDefault();
+	    String returnValue = "";
+	    try {
+		HttpPost httppost = new HttpPost(getUrl());
 
-			/*		
-			File tempFile = File.createTempFile("prefix-", "-suffix");
-			//tempFile.deleteOnExit();
-			FileOutputStream out = new FileOutputStream(tempFile);
-			IOUtils.copy(getInputStream(), out);
-			out.flush();
-			*/
-			FileBody bin = new FileBody(getFile());
-			//FileBody bin = new FileBody(new File("c:\\Users\\mheene\\Documents\\project\\bufrdashboard\\test\\2bufr.bin"));
-			// filebox
-			HttpEntity reqEntity = MultipartEntityBuilder.create()
+		/*		
+				File tempFile = File.createTempFile("prefix-", "-suffix");
+				//tempFile.deleteOnExit();
+				FileOutputStream out = new FileOutputStream(tempFile);
+				IOUtils.copy(getInputStream(), out);
+				out.flush();
+		*/
+		FileBody bin = new FileBody(getFile());
+		//FileBody bin = new FileBody(new File("c:\\Users\\mheene\\Documents\\project\\bufrdashboard\\test\\2bufr.bin"));
+		// filebox
+		HttpEntity reqEntity = MultipartEntityBuilder.create()
                     .addPart(getFieldName(), bin)          
                     .build();
-			//FileBody bin = new FileBody(new File(args[0]));
-            //StringBody comment = new StringBody("A binary file of some kind", ContentType.TEXT_PLAIN);
-			/*
-            HttpEntity reqEntity = MultipartEntityBuilder.create()
-                    .addBinaryBody("filebox", getInputStream())          
-                    .build();
-			*/
-            httppost.setEntity(reqEntity);
+		//FileBody bin = new FileBody(new File(args[0]));
+		//StringBody comment = new StringBody("A binary file of some kind", ContentType.TEXT_PLAIN);
+		/*
+		  HttpEntity reqEntity = MultipartEntityBuilder.create()
+		  .addBinaryBody("filebox", getInputStream())          
+		  .build();
+		*/
+		httppost.setEntity(reqEntity);
 
-            System.out.println("executing request " + httppost.getRequestLine());
-            CloseableHttpResponse response = httpclient.execute(httppost);
-            try {
-                System.out.println("----------------------------------------");
-                System.out.println(response.getStatusLine());
-                HttpEntity resEntity = response.getEntity();
-                if (resEntity != null) {
-                    System.out.println("Response content length: " + resEntity.getContentLength());
-					String responseString = EntityUtils.toString(resEntity, "UTF-8");
-					System.out.println(responseString);
-					returnValue = responseString;
+		System.out.println("executing request " + httppost.getRequestLine());
+		CloseableHttpResponse response = httpclient.execute(httppost);
+		try {
+		    System.out.println("----------------------------------------");
+		    System.out.println(response.getStatusLine());
+		    HttpEntity resEntity = response.getEntity();
+		    if (resEntity != null) {
+			System.out.println("Response content length: " + resEntity.getContentLength());
+			String responseString = EntityUtils.toString(resEntity, "UTF-8");
+			System.out.println(responseString);
+			returnValue = responseString;
 					
-                }
-                EntityUtils.consume(resEntity);
-            } finally {
-                response.close();
-            }
-        } finally {
-            httpclient.close();
-        }
-		return returnValue;
-		/*	
+		    }
+		    EntityUtils.consume(resEntity);
+		} finally {
+		    response.close();
+		}
+	    } finally {
+		httpclient.close();
+	    }
+	    return returnValue;
+	    /*	
 		HttpClient client = new DefaultHttpClient();
-			HttpPost post = new HttpPost(this.getUrl());
-			//String textFileName = null;
-			//File file = new File(textFileName);
-			InputStream ios = getInputStream();
-			MultipartEntityBuilder builder = MultipartEntityBuilder.create();
-			builder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
-			builder.addBinaryBody("filebox", ios, ContentType.MULTIPART_FORM_DATA, getFileName());
+		HttpPost post = new HttpPost(this.getUrl());
+		//String textFileName = null;
+		//File file = new File(textFileName);
+		InputStream ios = getInputStream();
+		MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+		builder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
+		builder.addBinaryBody("filebox", ios, ContentType.MULTIPART_FORM_DATA, getFileName());
 			
-			// 
-			HttpEntity entity = builder.build();
-			post.setEntity(entity);
-			HttpResponse response = client.execute(post);	
-			return response.toString();
-		*/	
+		// 
+		HttpEntity entity = builder.build();
+		post.setEntity(entity);
+		HttpResponse response = client.execute(post);	
+		return response.toString();
+	    */	
         }
     }
 }
